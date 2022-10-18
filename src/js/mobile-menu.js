1c665160742b78c8f6f54cf2ddb6348d19541312
AleksPastukhov <passandro1990@gmail.com>
2022-10-18 13:46:17 +0300

    править баг (закривання меню при натисканні кнопки) моб меню

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -2,6 +2,10 @@
   const mobileMenu = document.querySelector('.js-menu-container');
   const openMenuBtn = document.querySelector('.js-open-menu');
   const closeMenuBtn = document.querySelector('.js-close-menu');
+  const closeMenuBtnHome = document.querySelector('.js-close-menu-home');
+  const closeMenuBtnAbout = document.querySelector('.js-close-menu-about');
+  const closeMenuBtnProducts = document.querySelector('.js-close-menu-products');
+  const closeMenuBtnContacts = document.querySelector('.js-close-menu-contacts');
 
   const toggleMenu = () => {
     const isMenuOpen =
@@ -17,6 +21,10 @@
 
   openMenuBtn.addEventListener('click', toggleMenu);
   closeMenuBtn.addEventListener('click', toggleMenu);
+  closeMenuBtnHome.addEventListener('click', toggleMenu);
+  closeMenuBtnAbout.addEventListener('click', toggleMenu);
+  closeMenuBtnProducts.addEventListener('click', toggleMenu);
+  closeMenuBtnContacts.addEventListener('click', toggleMenu);
 
   // Close the mobile menu on wider screens if the device orientation changes
   window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
