696f0ff2a28d4427798383681e2cf014f17726e0
Slava Antolyk <Sl.antolyk@gmail.com>
2022-10-19 23:40:28 +0300

    Add desktop version

--- src/js/modal-ing-milk.js
+++ src/js/modal-ing-milk.js
@@ -2,7 +2,7 @@
   const refs = {
     openModalBtn: document.querySelector('[data-modal-ing-milk-open]'),
     closeModalBtn: document.querySelector('[data-modal-ing-milk-close]'),
-    modal: document.querySelector('[data-modal-milk-ice]'),
+    modal: document.querySelector('[data-modal-ing-milk]'),
   };
 
   refs.openModalBtn.addEventListener('click', toggleModal);
