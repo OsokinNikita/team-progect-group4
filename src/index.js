18bed4dd19a0957bf2db49a6787720fb24139d9f
nastiaknik <anastazja.knihnitska@gmail.com>
2022-10-20 09:49:30 +0200

    Fixed counter

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import './js/mobile-menu';
 import './js/modal-buy';
 import './js/anchor-scroll';
 import './js/header-scrol';
-import './js/increasing-numbers';
 import './js/btn-up';
 import './js/reviews-pagination';
+import './js/counter-hero';
+import './js/counter-advantages';
